2589cb827d5175b1a19a71cbca0c5f0b1b198d43
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-02 01:04:58 +0600

    double equal vs triple equal

---
+++ double-vs-triple.js
@@ -0,0 +1,55 @@
+// let num1 = 2;
+// let num2 = 2;
+// if (num1 == num2) {
+//   console.log("true"); // 2==2 beacause value same
+// } else {
+//   console.log("false");
+// }
+
+// let num1 = 2;
+// let num2 = "2";
+// if (num1 === num2) {
+//   console.log("true");
+// } else {
+//   console.log("false"); // 2==="2" false value same but data type not same
+// }
+
+// let first = 0;
+// let second = false; // false =0 and true =1;
+// if (first == second) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// let first = 0;
+// let second = false; // false =0 and true =1;
+// if (first == second) {
+//   console.log("true"); // true beacause value same
+// } else {
+//   console.log("false");
+// }
+
+// let first = 0;
+// let second = false; // false =0 and true =1;
+// if (first === second) {
+//   console.log("true");
+// } else {
+//   console.log("false");// false beacause value same but date type not same
+// }
+
+// let first = 1;
+// let second = true; // false =0 and true =1;
+// if (first == second) {
+//   console.log("true"); // true beacause value same
+// } else {
+//   console.log("false");
+// }
+
+let first = 1;
+let second = true; // false =0 and true =1;
+if (first === second) {
+  console.log("true");
+} else {
+  console.log("false"); // false beacause value same but date type not same
+}
