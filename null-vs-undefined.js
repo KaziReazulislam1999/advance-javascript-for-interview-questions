877d1aee57e908d623c75bea6adc6663da713c3c
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-02 00:26:46 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,46 @@
+// let names;
+// console.log(names);
+
+// function add(num1, num2) {
+//   const addition = num1 + num2;
+//   //   return addition;  [ Return Na Kora Holey Result Undifined]
+// }
+// const result = add(1, 2);
+// console.log(result);
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     // undefined [Return Na Korley Undefined Dekhabey]
+// }
+
+// const result = add(1, 2);
+// console.log(result);
+
+// function add(num1, num2) {
+//   console.log(num1, num2);
+// }
+
+// const result = add(1); // num1= 1(defined) but num2 = undefined;
+// console.log(result);
+
+// const studentInfo = {
+//   Name: "Reazul",
+//   Age: 21,
+//   SchoolName: "Shohid Titumir School",
+//   ClassRoll: 1448,
+// };
+// console.log(studentInfo.Name);  /// Eta Defined Kora Hoyeyse
+
+// const studentInfo = {
+//   Name: "Reazul",
+//   Age: 21,
+//   SchoolName: "Shohid Titumir School",
+//   ClassRoll: 1448,
+// };
+// console.log(studentInfo.PhoneNumber); /// Eta ta Object er value jeta choaoa hoise oita oi object er vitor nai tai undefined;
+
+// let fun = undefined;
+// console.log(fun); // undefined
+
+let age = [1, 2, 4, 55, 21, 31];
+console.log(age[66]);
