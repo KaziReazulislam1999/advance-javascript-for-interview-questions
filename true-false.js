d954636c95fc79f5279aa112d1d74d473781a3fc
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-01 23:57:38 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,53 @@
+// Falsy Values
+// 0
+// ""
+// undifined
+// null
+// NaN
+
+// Truthy Values
+// " "
+// "1","0","reazul","false"
+// [],{}
+
+// let age = 0;
+// if (age) {
+//   console.log("Condition Is True");
+// } else {
+//   console.log("Condition Is False");
+// }
+
+// let age = -4;
+// if (age > 0) {
+//   console.log("Condition Is True");
+// } else {
+//   console.log("Condition Is False");
+// }
+
+// let names = "reazul";
+// if (names.length > 0) {
+//   console.log("Condition Is True");
+// } else {
+//   console.log("Condition Is False");
+// }
+
+// let names = "";
+// if (names) {
+//   console.log("Condition Is True");
+// } else {
+//   console.log("Condition Is False");
+// }
+
+// let names = null;
+// if (names) {
+//   console.log("Condition Is True");
+// } else {
+//   console.log("Condition Is False");
+// }
+
+let names = 0;
+if (names || names == 0) {
+  console.log("Condition Is True");
+} else {
+  console.log("Condition Is False");
+}
