a0aa4ce326ca3598267a19c1697ab3bdc71c9721
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-02 15:51:53 +0600

    Arrow Function Part 01

---
+++ arrow-function-1.js
@@ -0,0 +1,33 @@
+// function display1() {
+//   console.log("I Am Display One");
+// }
+// display1();
+
+// const display2 = () => {
+//   console.log("I Am Display Two");
+// };
+// display2();
+
+// // Traditional Function
+// function message1() {
+//   return "I am message one";
+// }
+// console.log(message1());
+
+// // Arrow Function
+// const message2 = () => {
+//   return "I am Message Two";
+// };
+// console.log(message2());
+
+// const message3 = () => "I am Message Three";
+// console.log(message3());
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// const result = add(20, 11);
+// console.log(result);
+
+const add2 = (num1, num2) => num1 + num2;
+console.log(add2(22, 12));
